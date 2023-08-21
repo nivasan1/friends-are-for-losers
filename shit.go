91d781a2b57c1e343dd142ee100210fb1d50aebf
dw <dsw6ru@virginia.edu>
2023-08-21 00:39:59 -0400

    more

--- shit.go
+++ shit.go
@@ -3,85 +3,96 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/big"
 	"os"
-	"time"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/joho/godotenv"
-	"net/http"
-	"io/ioutil"
-	"encoding/json"
+	"context"
 )
 
-var (
-	ENDPOINT_URL    string
-	PRIVATE_KEY     string
-	PUBLIC_ADDR     string
-	CONTRACT_ADDRESS = common.HexToAddress("0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4")
-	hexcode         = "0x6945b123000000000000000000000000"
-	u256            = "0000000000000000000000000000000000000000000000000000000000000001"
+const (
+	ENDPOINT_URL  = "YOUR_ETHEREUM_NODE_ENDPOINT"  // Replace this with your Ethereum RPC URL
+	PRIVATE_KEY   = "YOUR_PRIVATE_KEY"             // Replace this with your Ethereum account private key
+	PUBLIC_ADDR   = "YOUR_PUBLIC_ADDRESS"          // Replace this with your Ethereum public address
 )
-
-type Target struct {
-	Address string `json:"address"`
-	TwitterUsername string `json:"twitterUsername"`
-}
 
 func loadEnv() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
-
-	ENDPOINT_URL = os.Getenv("ENDPOINT_URL")
-	PRIVATE_KEY = os.Getenv("PRIVATE_KEY")
-	PUBLIC_ADDR = os.Getenv("PUBLIC_KEY")
 }
 
-func userToAddr(user string) (string, error) {
-	resp, err := http.Get("https://prod-api.kosetto.com/search/users?username=" + user)
+func userToAddr(userName string) (string, error) {
+	// Here, you would have some logic to map usernames to Ethereum addresses.
+	// Just for illustration purposes, we'll assume a simple case.
+	switch userName {
+	case "testUser":
+		return "0xSomeEthereumAddressHere", nil
+	default:
+		return "", fmt.Errorf("User not found")
+	}
+}
+
+func makeAuth(privKeyHex, accountAddress string) (*bind.TransactOpts, error) {
+	privateKey, err := crypto.HexToECDSA(privKeyHex)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.From = common.HexToAddress(accountAddress)
+	auth.Nonce = nil     // Set nil to use pending nonce
+	auth.Signer = func(address common.Address, tx *types.Transaction) (*types.Transaction, error) {
+		return types.SignTx(tx, types.NewEIP155Signer(big.NewInt(1337)), privateKey) // Note: replace 1337 with your chain ID
+	}
+	auth.Value = big.NewInt(0) // in wei
+	auth.GasLimit = uint64(3000000) // in units
+	auth.GasPrice = big.NewInt(20000000000) // in wei
+
+	return auth, nil
+}
+
+func sendTransaction(client *ethclient.Client, toAddress common.Address, auth *bind.TransactOpts) error {
+	rawTx := types.NewTransaction(auth.Nonce.Uint64(), toAddress, auth.Value, auth.GasLimit, auth.GasPrice, nil)
+
+	signedTx, err := auth.Signer(auth.From, rawTx)
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	var targets []Target
-	err = json.Unmarshal(body, &targets)
-	if err != nil {
-		return "", err
-	}
-
-	for _, target := range targets {
-		if target.TwitterUsername == user {
-			return target.Address, nil
-		}
-	}
-
-	return "", nil
+	return client.SendTransaction(context.TODO(), signedTx)
 }
 
 func main() {
 	loadEnv()
+
 	client, err := ethclient.Dial(ENDPOINT_URL)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	// TODO: Continue with Ethereum logic using go-ethereum
-
-	// For demonstration purposes, using the userToAddr function
 	address, err := userToAddr("testUser")
 	if err != nil {
 		log.Fatalf("Failed to get address: %v", err)
 	}
-	fmt.Println(address)
+
+	toAddress := common.HexToAddress(address)
+	auth, err := makeAuth(PRIVATE_KEY, PUBLIC_ADDR)
+	if err != nil {
+		log.Fatalf("Failed to create auth: %v", err)
+	}
+
+	err = sendTransaction(client, toAddress, auth)
+	if err != nil {
+		log.Fatalf("Failed to send transaction: %v", err)
+	}
+
+	fmt.Println("Transaction sent to address:", address)
 }
 
-// TODO: Add Ethereum functions, logic for buying shares, etc.
